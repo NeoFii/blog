c329ca4d723a9d8f8cf805c95c52d4f4ece95675
NeoFii <732779870@qq.com>
2026-04-14 12:34:20 +0800

    update variable name

--- tina/config.ts
+++ tina/config.ts
@@ -6,8 +6,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
 	branch,
-	clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-	token: process.env.TINA_TOKEN,
+	clientId: process.env.TINA_PUBLIC_CLIENT_ID,
+token: process.env.TINA_TOKEN,
 
 	build: {
 		outputFolder: 'admin',
